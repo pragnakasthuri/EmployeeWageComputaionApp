521434e7d64daf408062541878a0140ab976c431
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 22:08:25 +0530

    Check Employee is Present or Absent code is added

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,13 @@
+/**
+ * Check Employee is Present or Absent
+ */
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("Check Employee is Present or Absent");
+        Employee employee = new Employee();
+        System.out.println("Employee is "+(employee.isPresent() ? "Present" : "Absent"));
     }
 }
