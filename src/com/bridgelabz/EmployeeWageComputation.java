3fd09413324a32d66e80084611dc0646063b0ec7
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 22:54:26 +0530

    Adding code for Calculating Employee monthly wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,5 +1,5 @@
 /**
- * Calculate Part Time  and full time employee wage using switch case
+ * calculate monthly wage
  */
 package com.bridgelabz;
 
@@ -8,22 +8,15 @@ public class EmployeeWageComputation {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         /**
-         * passing values through constructor and calculating employee daily wage
+         * passing values through constructor and calculating employee monthly wage
          */
         Employee fullTimeEmployee = new Employee(EmployeeType.FULL_TIME, 8, 20);
-        Employee partTimeEmployee = new Employee(EmployeeType.PART_TIME, 4, 20);
-        /**
-         * Using switch case to calculate full time and parttime employee wage
-         */
-        switch (fullTimeEmployee.getEmployeeType()) {
-            case FULL_TIME:
-                System.out.println("Full time Employee Daily Wage : " + fullTimeEmployee.getEmpHrs() * fullTimeEmployee.getWagePerHr());
-                break;
-            case PART_TIME:
-                System.out.println("Part time Employee Daily Wage : " + partTimeEmployee.getEmpHrs() * partTimeEmployee.getWagePerHr());
-                break;
-            default:
-                System.out.println("Invalid employee type!");
+        int monthlyWage = 0;
+        for (int i = 1; i <= 20; i++) {
+            if (fullTimeEmployee.isPresent()) {
+                monthlyWage += (fullTimeEmployee.getEmpHrs() * fullTimeEmployee.getWagePerHr());
+            }
         }
+        System.out.println("Employee Monthly wage: $" + monthlyWage);
     }
 }
