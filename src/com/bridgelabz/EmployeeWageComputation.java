88717b0efcde7b77f1f57b3e20ca545a68482d3f
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 23:00:44 +0530

    Adding code for Calculating employee wage for a given condition

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,22 +1,39 @@
 /**
- * calculate monthly wage
+ * Calculate Wages till a condition of
+ * total working hours or days is reached for a month
  */
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+    /**
+     * Declaring constants
+     */
+    private static final int MAX_NUM_OF_HRS = 100;
+    private static final int FUL_TIME_HRS = 8;
+    private static final int PART_TIME_HRS = 4;
+    private static final int WAGE_PER_HR = 20;
+    private static final int MAX_NO_OF_WORKING_DAYS = 20;
 
+    public static void main(String[] args) {
         /**
-         * passing values through constructor and calculating employee monthly wage
+         * passing values through constructor and using while loop for checking condition
          */
-        Employee fullTimeEmployee = new Employee(EmployeeType.FULL_TIME, 8, 20);
-        int monthlyWage = 0;
-        for (int i = 1; i <= 20; i++) {
-            if (fullTimeEmployee.isPresent()) {
-                monthlyWage += (fullTimeEmployee.getEmpHrs() * fullTimeEmployee.getWagePerHr());
+        Employee employee = new Employee(EmployeeType.FULL_TIME, 8, 20);
+        while (employee.getEmpHrs() <= MAX_NUM_OF_HRS && employee.getTotalNoOfWorkingDays() <= MAX_NO_OF_WORKING_DAYS ){
+            if (employee.isPresent()) {
+                switch (employee.getEmployeeType()) {
+                    case FULL_TIME:
+                        employee.setEmpHrs(employee.getEmpHrs() + FUL_TIME_HRS);
+                        break;
+                    case PART_TIME:
+                        employee.setEmpHrs(employee.getEmpHrs() + PART_TIME_HRS);
+                        break;
+                    default:
+                        System.out.println("Invalid employee type!");
+                        break;
+                }
             }
         }
-        System.out.println("Employee Monthly wage: $" + monthlyWage);
+        System.out.println("Employee Monthly Wage : " + employee.getEmpHrs() * WAGE_PER_HR);
     }
 }
