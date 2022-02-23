772d94a5cef0a39584b40dc79d63c8db5c15c3af
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 22:43:06 +0530

    Employee daily wage using switch case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,18 +1,29 @@
 /**
- * Calculate Part Time employee wage
+ * Calculate Part Time  and full time employee wage using switch case
  */
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        System.out.println("Check Employee is Present or Absent");
+
         /**
          * passing values through constructor and calculating employee daily wage
          */
-        Employee employee = new Employee(EmployeeType.PART_TIME, 4, 20);
-        System.out.println("Employee is "+(employee.isPresent() ? "Present" : "Absent"));
-
-        System.out.println("Part Time Daily Employee Wage: "+employee.getEmpHrs() * employee.getWagePerHr());
+        Employee fullTimeEmployee = new Employee(EmployeeType.FULL_TIME, 8, 20);
+        Employee partTimeEmployee = new Employee(EmployeeType.PART_TIME, 4, 20);
+        /**
+         * Using switch case to calculate full time and parttime employee wage
+         */
+        switch (fullTimeEmployee.getEmployeeType()) {
+            case FULL_TIME:
+                System.out.println("Full time Employee Daily Wage : " + fullTimeEmployee.getEmpHrs() * fullTimeEmployee.getWagePerHr());
+                break;
+            case PART_TIME:
+                System.out.println("Part time Employee Daily Wage : " + partTimeEmployee.getEmpHrs() * partTimeEmployee.getWagePerHr());
+                break;
+            default:
+                System.out.println("Invalid employee type!");
+        }
     }
 }
